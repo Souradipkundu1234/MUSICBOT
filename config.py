5292119db6e0d5d38fe8b0c3b504ac78e996983f
Souradipkundu1234 <kundusouradip568@gmail.com>
2026-01-01 22:58:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+YT_API_KEY = getenv("YT_API_KEY" , 'StrangerApidc3ee10' ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TOXICPAPA00/NORMAL_MUSIC")
